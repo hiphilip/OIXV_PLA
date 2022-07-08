06423cf88e92b827d028d215bbb9bb2536647cfa
hiphilip <95500131+hiphilip@users.noreply.github.com>
2022-07-08 14:51:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atinea_OI_XV_PLA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wys = new List<int>();
+            string line;
+            int n = Int32.Parse(Console.ReadLine());
+            while (!String.IsNullOrEmpty(line=Console.ReadLine())) 
+            {
+                string[] a = line.Split();
+                wys.Add(Int32.Parse(a[1]));
+            }
+            
+            List<int> s = new List<int>();
+            int p=0;
+            s.Add(0);           // zainicjowanie "pustej" listy stosu
+            
+
+            for(int i = 0; i < n; i++)
+            {
+                while (s.Count!=0 & s[0]>wys[i])
+                {
+                    s.RemoveAt(0);
+                }
+                if(s.Count==0 ^ s[0]<wys[i])
+                {
+                    s.Insert(0,wys[i]);
+                    p++;
+                }
+            }
+
+            Console.WriteLine(p); 
+        }
+    }
+}
